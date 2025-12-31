25a2196e1fe63ce91c34b2cb0e9165b98c653dd4
Guilherme Ranoya <ranoya@gmail.com>
2025-12-30 22:56:25 -0300

    novas funções

--- dom.js
+++ dom.js
@@ -4,7 +4,6 @@
 
 */
 
-
 /*
 
     Get Element By Anything
@@ -21,7 +20,7 @@
 
 const $ = function (str) {
   return document.querySelector(str);
-}
+};
 
 /*
 
@@ -40,8 +39,8 @@ const $ = function (str) {
 */
 
 const $$ = function (str) {
-  return document.querySelectorAll(str)
-}
+  return document.querySelectorAll(str);
+};
 
 /*
 
@@ -53,18 +52,77 @@ const $$ = function (str) {
     mydata2 = $_GET['nda'];
 
 */
-  
+
 let $_GET = [];
-(function(){
-  corte = window.location.href.toString().indexOf('?');
+(function () {
+  corte = window.location.href.toString().indexOf("?");
   if (corte > 0) {
-      argumento = window.location.href.toString().substring(corte + 1);
-      argumentos = argumento.split('&');
-      for (arg in argumentos){
-          let argCorte = argumentos[arg].indexOf('=');
-          $_GET[argumentos[arg].substring(0,argCorte)] = argumentos[arg].substring(argCorte + 1);
-      }
-  } 
+    argumento = window.location.href.toString().substring(corte + 1);
+    argumentos = argumento.split("&");
+    for (arg in argumentos) {
+      let argCorte = argumentos[arg].indexOf("=");
+      $_GET[argumentos[arg].substring(0, argCorte)] = argumentos[arg].substring(
+        argCorte + 1
+      );
+    }
+  }
 })();
-  
-  
+
+/*
+
+    Swap DOM Elements
+
+    Example:
+    domSwap( $("#myid"), $("#myid2") );
+
+*/
+
+let domSwap = function (obj1, obj2) {
+  var temp = document.createElement("div");
+  obj1.parentNode.insertBefore(temp, obj1);
+  obj2.parentNode.insertBefore(obj1, obj2);
+  temp.parentNode.insertBefore(obj2, temp);
+  temp.parentNode.removeChild(temp);
+};
+
+/*
+   
+   Create new DOM Element(s) and return
+   a array of created elements or a single element
+
+    domNew( HTML Tag, Properties Array, Element Content, Elements to insert the new elements );
+    or
+    array = domNew( HTML Tag, Properties Array, Element Content, Elements to insert the new elements );
+
+    Examples:
+
+    manyelements = domNew("div", { id: "myid", class: "myclass" }, "inner HTML", ".multipleexistingelements");
+    manyelements[2].innerHTML = "changed inner HTML";
+
+    myelement = domNew("p", {}, "justa a paragraph inside body");
+    myelement.innerHTML = "changed the content of the paragraph";
+
+*/
+
+let domNew = function (tag, attrs = {}, inner = "", where) {
+  let element = document.createElement(tag);
+
+  for (let key in attrs) {
+    element.setAttribute(key, attrs[key]);
+  }
+
+  element.innerHTML = inner;
+
+  let matrix = [];
+
+  if (typeof where != "undefined" && where != null && where != "") {
+    let places = $$(where);
+    for (let i = 0; i < places.length; i++) {
+      matrix[i] = places[i].appendChild(element.cloneNode(true));
+    }
+  } else {
+    matrix = document.body.appendChild(element);
+  }
+
+  return matrix;
+};
